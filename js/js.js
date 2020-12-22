232bcef40133fc94ea73a8c574730ef8bd251605
illionillion <yuto.0526.sano@gmail.com>
2020-12-22 20:53:01 +0900

    add new function

--- js/js.js
+++ js/js.js
@@ -21,6 +21,12 @@ var text2={
   "家の外に出る":[["折角だし、散歩がてらコンビニか何かで軽く食べられるものを買いに行こうと思う。","玄関の扉を開いて、外へ出る。<br>外は寒々しい風が吹いていた。","もう少し厚着をすればよかったかなとも思ったが、コンビニは結構近くにあるし、別にいいやと思い、足を進めた。","道の隅にはイチョウが集まっていて、秋はもう終わっているのだと気づく。<br>顔を上に向けたら、曇った空が広がっていた。","コンビニについて、俺は温かいカフェオレとそれに合いそうなクッキーを購入した。<br>クッキーにはチョコチップが混ぜられている。","レジに通して、コンビニを出た。<br>寒さはより一層強くなっているような気がした。","俺は温かいカフェオレの缶を手で持っていた。<br>家に帰ってから飲もうとしていたが、あまりの寒さにそのタブを開いた。","カフェオレの甘い味が口の中に広がる。","大して歩いているわけでもないのに、長い長い雪道を歩いている気がしてならない。","ふと、公園の隣を通りかかった。","そこには、屋根付きのベンチがある。","大して疲れているわけでもない。<br>家もすぐ近くなのに俺はその公園に入り、ベンチに座った。","手に持っているカフェオレを呷る。","最初に飲んだ時よりも少し冷めているのがわかった。","いつの間にか、雪が降り始めた。","俺は身体の気怠さを感じつつ雪が降りてくる空を見上げていた。","俺はなんでこんなことをしているのだろう…。"],["勉強をする","勉強をしない"]]
 };
 
+var endtext={
+  "ベットの下を覗き込む":[["桜が散っていた。","何本も咲いている桜の花のトンネルは今年の新入生を歓迎しているようだった。","真新しいスーツを着ている僕は入学式が行われる会場に向かっていた。","今は何故俺がこんなにも勉強に熱中して、地元ではそこそこ有名な大学に入学できたのかも覚えていない。","何かキッカケがあったような気がするが、今はもう辛かった勉強の日々が思い出される。<br>今はもうそれから解き放たれたのだ。<br>大学生活を楽しみたい。","きっとこれからは、新たな出会いがたくさんあるのだろう。","「ねぇ、君！」","ふと、後ろから俺に向けて声がかけられた。<br>俺はその呼びかけに振り返った。","そこにいたのは、あの日、卒業アルバムにいた…！","[大学入学エンド]"],["寒い夜道を歩いていた。<br>行く当てもないけれど、ただ立ち止まることだけはできなかった。","立ち止まっていても、今の自分を救ってくれる人はいないのだから。","勉強もせず、ただ見栄を張って学力の高い大学を受けた俺は、見事に全滅した。","就職に変更することなども考えたが、周りに流されるまま、結局俺は浪人することにした","けれど、それは間違いだったのかもしれない。","勉強をしつつ、アルバイトを始めた俺はバイト先で知り合った人に賭け事に誘われて、…そしてハマった。<br>何も言い訳ができないくらいにハマってしまったのだ。","競馬、麻雀、パチンコ…。<br>最初のうちはかなり当たったりして、羽振りが良くなったのだが、いつの間にかただただ財布の中身はどんどん減っていった。","いつしか、親に土下座までしてお金を貰うようになっていた。","…まあ、今はその親に家を追い出されているわけだけど。","まあ、バイト先の友人に頼んだら、笑い話にして、家にあげてくれるだろう。<br>俺は彼の家へ向かって歩いていた。","ふと、下を向いていたせいか、前から来ていた人とぶつかる。","「あ、すいません。大丈夫ですか？」","俺は彼女の顔を見て、ハッと目を見開いた。","そう、俺が中学時代に片想いをしていた子だった。<br>服装や荷物からして、どこかの大学にでも通っているのだろうか。","喉から出かかっていた『久しぶり』の言葉を呑み込んで、俺は一礼してすぐにその場を去った。","「ちゃんと気をつけてくださいね！」","後ろにいる彼女はそんな優しい言葉を投げかけてくれた。","彼女は優しくてとてもいい子だから、俺みたいな愚かでどうしようもない人間とは関わるべきではないだろう。","俺は振り返らず、戻りたい過去さえからも逃げて、歩き続けた。","[浪人エンド]"]],
+  "本棚から漫画を取る":[["a","b","c"],["運命の時が、近づいていた。","時計の長針があと一度動いたら、このパソコンの画面から結果が出てくる。","思えば、今まで本当に長かった。","俺は学校の勉強をおざなりにして、漫画の勉強と創作に打ち込んだ。<br>学校の先生が助けてくれなかったら、俺は卒業できていただろうか。","俺は専門学校で絵や物語の作り方などを学び、真剣に創作を学んだ。<br>辛いことも何度もあって、何度筆を折ろうと思ったが、それでも漫画が好きだという気持ちでここまでやってきた。","そして今日、自分の自信作の漫画が新人賞を受賞できるかの発表の日だった。","自分の原稿は最終選考まで残っている。","たとえ、受賞できなくても、自分の作品がよければ編集者さんについてもらうこともできる。<br>俺はそれを一生懸命待ち続けていた。","部屋の時計がカチ、と鳴って長針が動いた。","俺はマウスをクリックして、結果発表のページを開く。","そこには…！","［漫画家デビューエンド]"]],
+  "":[[],[]]
+  };
+
 var sentence;
 var option;
 var special;
@@ -40,6 +46,17 @@ window.onload=function title(){
       special=text2[sessionStorage.getItem('part-count')][2];
     }else{
       special=[];
+    }
+    if(sessionStorage.hasOwnProperty('end-count')){
+      switch(sessionStorage.getItem('end-count')){
+        case '勉強をする':
+          sentence=endtext[sessionStorage.getItem('part-count')][0];
+        break;
+        case '勉強をしない':
+          sentence=endtext[sessionStorage.getItem('part-count')][1];
+        break;
+      }
+      
     }
   }else{
     special=[];
@@ -80,35 +97,77 @@ nextbtn.onclick=function next(){
     
   }else{
     // location.reload();
-    nextbtn.classList.add('none');
-    block.classList.remove('none');
-    // yesbtn.classList.remove('none');
-    yesbtn.value=option[0];
-    // nobtn.classList.remove('none');
-    nobtn.value=option[1];
-    sessionStorage.setItem('page-count',page-1);
+
+    
+
+    if(sessionStorage.hasOwnProperty('end-count')){
+
+      if(nextbtn.value=="終わり"){
+            sessionStorage.clear();
+            location.reload();
+          }
+      nextbtn.value="終わり";
+    }else{
+      nextbtn.classList.add('none');
+      block.classList.remove('none');
+      // yesbtn.classList.remove('none');
+      yesbtn.value=option[0];
+      // nobtn.classList.remove('none');
+      nobtn.value=option[1];
+      sessionStorage.setItem('page-count',page-1);
+    }    
   }
   
+}
+
+function sleep(waitMsec) {
+  var startMsec = new Date();
+  
+  // 指定ミリ秒間だけループさせる（CPUは常にビジー状態）
+  while (new Date() - startMsec < waitMsec){
+    screen.innerHTML="";
+  };
 }
 
 yesbtn.onclick=function yes(){
   let root=yesbtn.value;
   // if(key in dic){console.log("あるよ")}
-  sessionStorage.clear('page-count');
-  if(text2[root]){
-  sentence=text2[root][0]; 
-  option=text2[root][1];
-  if(text2[root][2]){
-    special=text2[root][2];
-  }
-  nextbtn.classList.remove('none');
-  nextbtn.click();
-  block.classList.add('none');
-  // yesbtn.classList.add('none');
-  // nobtn.classList.add('none');
-  sessionStorage.setItem('part-count',root);
+  sessionStorage.removeItem('page-count');
+  
+  if(root=="勉強をする"){
+
+    if(endtext[sessionStorage.getItem('part-count')]){
+      sentence=endtext[sessionStorage.getItem('part-count')][0];
+      nextbtn.classList.remove('none');
+      
+      sleep(3000);
+      nextbtn.click();
+      block.classList.add('none');
+      sessionStorage.setItem('end-count',root);
+
+    }else{
+      sessionStorage.clear();
+      location.reload();
+    }
+
   }else{
-    location.reload();
+    if(text2[root]){
+    sentence=text2[root][0]; 
+    option=text2[root][1];
+      if(text2[root][2]){
+        special=text2[root][2];
+      }
+    nextbtn.classList.remove('none');
+    nextbtn.click();
+    block.classList.add('none');
+    // yesbtn.classList.add('none');
+    // nobtn.classList.add('none');
+    
+    sessionStorage.setItem('part-count',root);
+    }else{
+      sessionStorage.clear();
+      location.reload();
+    }
   }
     
 }
@@ -116,10 +175,32 @@ yesbtn.onclick=function yes(){
 nobtn.onclick=function no(){
   let root=nobtn.value;
 
-  sessionStorage.clear('page-count');
+  sessionStorage.removeItem('page-count');
+
+  if(root=="勉強をしない"){
+
+    if(endtext[sessionStorage.getItem('part-count')]){
+      sentence=endtext[sessionStorage.getItem('part-count')][1];
+      nextbtn.classList.remove('none');
+      
+      sleep(3000);
+      nextbtn.click();
+      block.classList.add('none');
+      sessionStorage.setItem('end-count',root);
+
+    }else{
+      sessionStorage.clear();
+      location.reload();
+    }
+
+  }else{
+
   if(text2[root]){
   sentence=text2[root][0];
   option=text2[root][1];
+    if(text2[root][2]){
+      special=text2[root][2];
+    }
   nextbtn.classList.remove('none');
   nextbtn.click();
   block.classList.add('none');
@@ -127,28 +208,11 @@ nobtn.onclick=function no(){
   // nobtn.classList.add('none');
   sessionStorage.setItem('part-count',root);
   }else{
+    sessionStorage.clear();
     location.reload();
   }
 }
-
-// function part_count(){
-//   if(sessionStorage.hasOwnProperty('part-count')){
-//     let count=sessionStorage.getItem('part-count');
-  
-//     // count=Number(count)+1;
-//     sessionStorage.setItem('part-count',count);
-    
-  
-//     console.log(count);
-//     return count;
-  
-//   }else{
-//     let count=0;
-//     // count=count+1;
-//     sessionStorage.setItem('part-count',count);
-//     return count;
-//   }
-// }
+}
 
 function page_count(){
 //何番目のフレーズかカウント
